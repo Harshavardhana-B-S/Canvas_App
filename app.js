edaed0a8579f8c66df9f22f61725e265ff2b44b6
Harshavardhana-B-S <harshabs113@gmail.com>
2023-11-19 21:17:25 +0530

    feat: Implement drag-and-drop functionality

--- app.js
+++ app.js
@@ -83,6 +83,14 @@ sticky.addEventListener("click",(e)=>{
     let textarea=stickyCont.querySelector(".text-area");
 
     notesActions(stickyCont,remove,minimize,textarea);
+
+    stickyCont.onmousedown = function (event) {
+        dragAndDrop(stickyCont, event);
+    };
+
+    stickyCont.ondragstart = function () {
+        return false;
+    };
 })
 
 
@@ -102,4 +110,34 @@ function notesActions(sticky_cont,remove,minimize,textarea){
     
     });
  
+}
+
+function dragAndDrop(element, event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+
+    moveAt(event.pageX, event.pageY);
+
+    // moves the ball at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // move the ball on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // drop the ball, remove unneeded handlers
+    element.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        element.onmouseup = null;
+    };
 }
